9ec58d920b116d418a0d41c982c1182f90cceac4
Glenn Cambre <glecam@bitconsult.be>
2018-08-12 13:20:54 +0200

    Modify server.js

--- Server.js
+++ Server.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const path = require('path');
 
+const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static('dist'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(`${__dirname  }/dist`));
 
-app.listen(8080, () => console.log('Listening on port 8080'));
+// send the user to index html page inspite of the url
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+app.listen(port);
